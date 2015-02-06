bd0f3454b88db2079b9d43c43fb83008fb4f72fd
Simon Bettison <simon@bettison.org>
2015-02-06 15:46:53 +0100

    Create cookbook, setup Berkshelf

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'cryptonet-opsworks'
+maintainer       "sgbett"
+maintainer_email "sgbett@remailer.org"
+license          'FreeBSD License'
+description      'Installs/Configures cryptonet-opsworks'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
