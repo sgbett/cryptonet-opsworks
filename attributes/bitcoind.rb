9d14dbc952515e3ddb22fefca95fd47858149582
Simon Bettison <simon@bettison.org>
2015-07-29 14:43:21 +0100

    version bump bitcoind to v0.11.0

--- attributes/bitcoind.rb
+++ attributes/bitcoind.rb
@@ -1,2 +1,2 @@
-default['cryptonet-opsworks']['bitcoind']['revision'] = "v0.10.2"
+default['cryptonet-opsworks']['bitcoind']['revision'] = "v0.11.0"
 default['cryptonet-opsworks']['bitcoind']['src_prefix'] = '/usr/local/src'
