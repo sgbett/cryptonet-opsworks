ee9b5c983f6112ea48205b624e8226ad085b2d04
Simon Bettison <simon@bettison.org>
2016-02-26 10:25:36 +0000

    updated to Bitcion Unlimited 0.12bu

--- attributes/bitcoind.rb
+++ attributes/bitcoind.rb
@@ -1,3 +1,3 @@
-default['cryptonet-opsworks']['bitcoind']['repo'] = "https://github.com/gandrewstone/BitcoinUnlimited.git"
-default['cryptonet-opsworks']['bitcoind']['revision'] = "0.11cfg_stats"
+default['cryptonet-opsworks']['bitcoind']['repo'] = "https://github.com/BitcoinUnlimited/BitcoinUnlimited.git"
+default['cryptonet-opsworks']['bitcoind']['revision'] = "0.12bu"
 default['cryptonet-opsworks']['bitcoind']['src_prefix'] = '/usr/local/src'
