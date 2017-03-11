f92222907ac808b94ef534785d7ac5d6fd217dc0
Simon Bettison <simon@bettison.org>
2017-03-11 13:04:09 +0000

    bump to BU 1.0.1.0

--- attributes/bitcoind.rb
+++ attributes/bitcoind.rb
@@ -1,3 +1,3 @@
 default['cryptonet-opsworks']['bitcoind']['repo'] = "https://github.com/BitcoinUnlimited/BitcoinUnlimited.git"
-default['cryptonet-opsworks']['bitcoind']['revision'] = "1.0.0.1"
+default['cryptonet-opsworks']['bitcoind']['revision'] = "1.0.1.0"
 default['cryptonet-opsworks']['bitcoind']['src_prefix'] = '/usr/local/src'
